cb59ef78d9d48c460489867d5c0c615f20b05e53
yanis-khiter <yanis.khiter@laplateforme.io>
2021-11-29 15:40:46 +0100

    test

--- connect.php
+++ connect.php
@@ -4,10 +4,10 @@
 
 
 // BASE PLESK
-$bdd = mysqli_connect('localhost', 'Yanis_Khiter', 'Maxi13013', 'yanis-khiter_moduleconnexion'); 
+// $bdd = mysqli_connect('localhost', 'Yanis_Khiter', 'Maxi13013', 'yanis-khiter_moduleconnexion'); 
 
 // MA BASE LOCALE
-// $bdd = mysqli_connect('localhost', 'root', '', 'yanis-khiter_moduleconnexion'); // 
+$bdd = mysqli_connect('localhost', 'root', '', 'yanis-khiter_moduleconnexion'); 
 
 mysqli_set_charset($bdd , 'utf8');
 
